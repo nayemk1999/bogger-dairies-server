30e0402dff8c5cf210a1fc5e1f65b7ab1bbe50fb
Nayem <nayemk900@gmail.com>
2021-06-25 21:39:06 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express')
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config()
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const port = 3003
+const {DB_NAME, DB_PASS, DB_USER} = process.env
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+const uri = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.mlivs.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const blogsCollection = client.db("blogger-dairies").collection("blogs");
+    const adminsCollection = client.db("blogger-dairies").collection("admins");
+
+    app.post('/addBlog', (req, res) => {
+        const newBlog = req.body
+        blogsCollection.insertOne(newBlog)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    });
+    app.post('/addAdmin', (req, res) => {
+        const newAdmin = req.body
+        adminsCollection.insertOne(newAdmin)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    });
+
+
+    app.get('/manageBlogs', (req, res) => {
+        const userEmail = req.query.email
+        blogsCollection.find({email: userEmail})
+            .toArray((error, document) => {
+                res.send(document)
+            })
+    })
+    app.get('/allBlogs', (req, res) => {
+
+        blogsCollection.find({})
+            .toArray((error, document) => {
+                res.send(document)
+            })
+    })
+    app.delete('/deleted/:id', (req, res) => {
+        const id = ObjectID(req.params.id)
+        blogsCollection.deleteOne({ _id: id })
+            .toArray((error, document) => {
+                res.send(document[0])
+            })
+    })
+    app.get('/blog/:id', (req, res) => {
+        const id = ObjectID(req.params.id)
+        blogsCollection.find({ _id: id })
+            .then(result => console.log(result))
+                // res.send(result.deletedCount > 0))
+    });
+
+});
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port || 3003)
